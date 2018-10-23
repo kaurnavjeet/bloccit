f93af0eb5c1a159386fcd7895e54f4e5d19d03ff
kaurnavjeet <kaurnavjeet09@gmail.com>
2018-10-23 16:01:10 -0700

    assignment express completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,11 +1,20 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marco = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
   describe("GET /", () => {
     it("should return status code 200", done => {
       request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+    });
+  });
+  describe("GET /marco", () => {
+    it("should return status code 200", done => {
+      request.get(marco, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         done();
       });
--- src/routes/static.js
+++ src/routes/static.js
@@ -4,5 +4,8 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   res.send("Welcome to Bloccit");
 });
+router.get("/marco", (req, res, next) => {
+  res.send("Polo");
+});
 
 module.exports = router;
