9ce33c7b87b20efb4fe288c1c94faf623244d45b
kaurnavjeet <kaurnavjeet09@gmail.com>
2018-10-24 10:52:17 -0700

    CRUD and seed implemented

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -31,6 +31,24 @@ module.exports = {
         callback(err);
       });
   },
+  updateTopic(id, updatedTopic, callback) {
+    return Topic.findById(id).then(topic => {
+      if (!topic) {
+        return callback("Topic not found");
+      }
+
+      topic
+        .update(updatedTopic, {
+          field: Object.keys(updatedTopic)
+        })
+        .then(() => {
+          callback(null, topic);
+        })
+        .catch(err => {
+          callback(err);
+        });
+    });
+  },
   deleteTopic(id, callback) {
     return Topic.destroy({
       where: { id }
