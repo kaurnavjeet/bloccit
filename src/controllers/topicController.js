d2c03c788b837af51e490a3d2a78c80dee7a1152
kaurnavjeet <kaurnavjeet09@gmail.com>
2018-10-24 00:49:14 -0700

    CR of CRUD applied

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -9,5 +9,30 @@ module.exports = {
         res.render("topics/index", { topics });
       }
     });
+  },
+  new(req, res, next) {
+    res.render("topics/new");
+  },
+  create(req, res, next) {
+    let newTopic = {
+      title: req.body.title,
+      description: req.body.description
+    };
+    topicQueries.addTopic(newTopic, (err, topic) => {
+      if (err) {
+        res.redirect(500, "/topic/new");
+      } else {
+        res.redirect(303, `/topics/${topic.id}`);
+      }
+    });
+  },
+  show(req, res, next) {
+    topicQueries.getTopic(req.params.id, (err, topic) => {
+      if (err || topic == null) {
+        res.redirect(404, "/");
+      } else {
+        res.render("topics/show", { topic });
+      }
+    });
   }
 };
