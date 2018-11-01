601cc63f309ef5fe9905c8bd962db9e9cd8b2549
kaurnavjeet <kaurnavjeet09@gmail.com>
2018-10-31 23:02:04 -0700

    assignment favoriting completed

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -69,6 +69,14 @@ module.exports = (sequelize, DataTypes) => {
         postId: post.id
       });
     });
+
+    Post.afterCreate((post, callback) => {
+      return models.Vote.create({
+        value: 1,
+        userId: post.userId,
+        postId: post.id
+      });
+    });
   };
   return Post;
 };
